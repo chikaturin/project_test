b244aff7d0518b48e7852858a5b5cffc4614e402
Chikaturin <ninhhoaithanh@gmail.com>
2024-07-17 00:43:13 +0700

    change env

--- Server/Database/data.js
+++ Server/Database/data.js
@@ -2,14 +2,14 @@ import mysql from 'mysql2/promise';
 
 async function initializeConnection() {
   const connection = await mysql.createConnection({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'DB_CNPM',
-    port: parseInt(process.env.DB_PORT, 10) || 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
   });
 
-  console.log('Successfully connected to MySQL database.');
+  console.log('Successful Connected to MySQL database.');
   return connection;
 }
 
