02b1637ddcddca8b39b56dc712854aecf51c7dad
Chikaturin <ninhhoaithanh@gmail.com>
2024-07-17 00:35:48 +0700

    change data1

--- Server/Database/data.js
+++ Server/Database/data.js
@@ -1,18 +1,18 @@
+
 import mysql from 'mysql2/promise';
 
 async function initializeConnection() {
-  try {
-    const connection = await mysql.createConnection({
-      host: process.env.DB_HOST || 'localhost',       // Sử dụng biến môi trường hoặc giá trị mặc định
-      user: process.env.DB_USER || 'root',            // Sử dụng biến môi trường hoặc giá trị mặc định
-      password: process.env.DB_PASSWORD || '',        // Sử dụng biến môi trường hoặc giá trị mặc định
-      database: process.env.DB_NAME || 'DB_CNPM',     // Sử dụng biến môi trường hoặc giá trị mặc định
-      port: parseInt(process.env.DB_PORT, 10) || 3306, // Sử dụng biến môi trường hoặc giá trị mặc định
-    });
-    console.log('Connected to the database');
-    return connection;
-  } catch (error) {
-    console.error('Error connecting to the database:', error.message);
-    throw error;  // Ném lỗi để ứng dụng có thể xử lý hoặc ghi log thêm
-  }
+  const connection = await mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'DB_CNPM'
+  });
+
+  console.log('Successful Connected to MySQL database.');
+  return connection;
 }
+
+const connection = await initializeConnection();
+
+export default connection;
