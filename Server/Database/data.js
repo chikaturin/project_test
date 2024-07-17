7e76ec29e2f107d1a03cf2bee7f23110fb5c2cf9
chikaturin <ninhhoaithanh@gmail.com>
2024-07-17 07:25:14 +0700

    huhu

--- Server/Database/data.js
+++ Server/Database/data.js
@@ -1,16 +1,15 @@
 import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
-    host: process.env.DB_HOST,  // Sửa lại với địa chỉ IP của cơ sở dữ liệu
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: 'localhost',  // Sửa lại với địa chỉ IP của cơ sở dữ liệu
+    user: 'root',
+    password: '',
+    database:'db_cnpm',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
 });
 
-// Hàm khởi tạo kết nối, không xuất khẩu
 export async function initializeConnection() {
     try {
         // Test một kết nối
--- Server/Server.mjs
+++ Server/Server.mjs
@@ -5,8 +5,9 @@ import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHt
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import { expressMiddleware } from '@apollo/server/express4';
-import {typeDefs} from './Schema_Resolve/typeDefs.js';
-import {resolvers} from './Schema_Resolve/resolvers.js';
+import { typeDefs } from './Schema_Resolve/typeDefs.js';
+import { resolvers } from './Schema_Resolve/resolvers.js';
+
 const app = express();
 const httpServer = http.createServer(app);
 
@@ -18,5 +19,7 @@ const server = new ApolloServer({
 
 await server.start();
 app.use(cors(), bodyParser.json(), expressMiddleware(server));
-await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
-console.log(`🚀 Server ready at http://localhost:4000`);
+
+const PORT = 4001;
+await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
+console.log(`🚀 Server ready at http://localhost:${PORT}`);
